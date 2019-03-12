27e9d5a76050f9e3f38b74c975f6a03657e5646e
Victor <victor.carranz@hotmail.com>
2019-03-12 16:55:11 -0600

    grid added :seedling:

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -4,5 +4,15 @@ const container = document.querySelector('.matriz');
 
 
 function createGrid(){
-    let box = document.createElement("div").classList;
+    
+    for(let j=0; j<size; j++){
+        let column =document.createElement('div');
+        for(let i=0; i<size; i++){
+            let box = document.createElement("div");
+            column.appendChild(box).classList.add('grid-item');
+        }
+        container.appendChild(column).classList.add('column-item');
+    }
 }
+
+createGrid();
